427090ea94bf40539b08192402a7b95ce476509e
ACHEHBOUNE Hamza <37669482+achehboune36@users.noreply.github.com>
2022-11-05 08:48:31 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+
+HEADER = 64
+PORT = 5050
+
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "server ip address"
+LINUX_PATH = "file path"
+new_line = "\n"
+
+REREAD_ON_QUERY = True
